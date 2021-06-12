76005b8eeb8b408541d38c95e41d9f6189385015
Sengolda <79252176+Sengolda@users.noreply.github.com>
2021-06-12 19:51:20 +0530

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,40 @@
+import math
+
+
+class Math:
+    @staticmethod
+    def add(x,y):
+        return x+y
+    @staticmethod
+    def remove(x,y):
+        return x-y
+    @staticmethod
+    def mulitiply(x,y):
+        return x*y
+    @staticmethod
+    def divide(x,y):
+        return x/y
+
+
+def calculate():
+    var = ("+","-","*","/")
+    take = input("\n".join(var))
+    if take == "+":
+        num1 = int(input(">"))
+        num2 = int(input(">"))
+        print(Math.add(num1,num2))
+    if take == "-":
+        num1 = int(input(">"))
+        num2 = int(input(">"))
+        print(Math.remove(num1,num2))
+    if take == "*":
+        num1 = int(input(">"))
+        num2 = int(input(">"))
+        print(Math.mulitiply(num1,num2))
+    if take == "/":
+        num1 = int(input(">"))
+        num2 = int(input(">"))
+        print(Math.divide(num1,num2))
+
+
+calculate()
